0f8827259ae653b0ab15f259138d15cf0251d704
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-04-15 23:28:43 -0400

    added some comments and basic report routes

--- harvest/__main__.py
+++ harvest/__main__.py
@@ -8,9 +8,24 @@ logger = startup.get_logger()
 
 app = Flask(__name__)
 
+# load modules
+from modules import ModuleLoader, ModuleRegister
+
+
+# start the webserver
 app.run()
 
 
 @app.route("/")
 def default() -> str:
     return 'hello world'
+
+
+@app.route("/reports/run")
+def reports_run(name: str, matches: tuple = (), add: tuple = (), exclude: tuple = (), limit: int = None) -> dict:
+    return {}
+
+
+@app.route("/reports/list")
+def reports_list() -> dict:
+    return {}
