ced17e72545dc9f93951627a8560a693a679ddc6
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-04-17 18:53:28 -0400

    added is_connected()
    added self.id, node, session, and _log_prefix
    
    updated logging statements

--- harvest/cache.py
+++ harvest/cache.py
@@ -8,18 +8,41 @@ logger = getLogger('harvest')
 
 
 class HarvestCacheConnection(MongoClient):
-    def __init__(self, **kwargs):
+    def __init__(self, node: str, **kwargs):
         super().__init__(**kwargs)
 
-        self.client = None
+        from uuid import uuid4
+        self.id = str(uuid4())
+        self.node = node
+        self.session = None
+
+        self._log_prefix = f'[{self.id}][{self.node}][{self.HOST}:{self.PORT}]'
 
     @property
     def is_connected(self) -> bool:
+        """
+        check if the connect is active
+        :return: bool
+        """
+
         try:
-            self.client
+            self.list_databases()
 
         except Exception as ex:
-            logger.debug('cache: not connection to backend')
+            logger.debug(f'{self._log_prefix}: ' + ' '.join(ex.args))
+            return False
+
+        else:
+            logger.debug(f'{self._log_prefix}: successful connection')
+            return True
+
     def connect(self):
-        self.client = self.start_session()
-        self.client.client.
+        # already connected; nothing else to do
+        if self.is_connected:
+            return self
+
+        self.session = self.start_session()
+
+
+
+        # you have to select a db/collection before querying
