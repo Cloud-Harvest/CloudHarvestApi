f950234b743f699b24f763d63bf2ebfbb168517e
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-04-25 16:24:54 -0400

    added option for result to be a float or int

--- harvest/test_utilities.py
+++ harvest/test_utilities.py
@@ -8,4 +8,4 @@ def test_duration_in_seconds():
     a = datetime(year=2023, month=1, day=1, hour=23, minute=30, second=0)
     b = datetime(year=2023, month=1, day=2, hour=0, minute=0, second=0)
 
-    assert duration_in_seconds(a=a, b=b) == 1800
+    assert duration_in_seconds(a=a, b=b) == 1800 or 1800.0
