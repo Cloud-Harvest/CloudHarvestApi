31ca9bd136a57486f95172e3ea8dde5e21ce3ee8
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-04-25 16:23:18 -0400

    added a file for basic functions we may reuse a lot

---
+++ harvest/test_utilities.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_duration_in_seconds():
+    from utilities import duration_in_seconds
+    from datetime import datetime
+
+    a = datetime(year=2023, month=1, day=1, hour=23, minute=30, second=0)
+    b = datetime(year=2023, month=1, day=2, hour=0, minute=0, second=0)
+
+    assert duration_in_seconds(a=a, b=b) == 1800
