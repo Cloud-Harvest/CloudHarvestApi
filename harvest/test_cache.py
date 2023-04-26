227074d94458de1e8eb1be8f9f041b1de8d5df05
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-04-26 15:53:15 -0400

    added tests for write_record() check_harvest_metadata()

--- harvest/test_cache.py
+++ harvest/test_cache.py
@@ -1,8 +1,14 @@
+import datetime
+
 import pytest
 from startup import load_configuration_files, load_cache_connections
 
 api_configuration = load_configuration_files()
 cache_nodes = load_cache_connections(cache_config=api_configuration['cache']['hosts'])
+
+
+def _load_test_records_json():
+    return open('harvest/test_data/test_records.json', 'r')
 
 
 def test_connection():
@@ -64,3 +70,61 @@ def test_duration_in_seconds():
     b = datetime(year=2023, month=1, day=2, hour=0, minute=0, second=0)
 
     assert cache_nodes['writer'].duration_in_seconds(a=a, b=b) == 1800 or 1800.0
+
+
+def test_get_collection_name():
+    from json import load
+    test_json = load(_load_test_records_json())
+
+    for record in test_json:
+        if record.get('Harvest'):
+            assert cache_nodes['writer'].get_collection_name(**record['Harvest']) == 'test_platform.test_service.test_type'
+
+
+def test_check_harvest_meta():
+    from json import load
+    test_json = load(_load_test_records_json())
+
+    for record in test_json:
+        assert cache_nodes['writer'].check_harvest_metadata(record.get('Harvest')) == record['expected_state']
+
+
+def test_write_record():
+    test_database = 'test'
+    now = datetime.datetime(year=2023, month=4, day=26, hour=19, minute=44, second=25)
+
+    from json import load
+    test_json = load(_load_test_records_json())
+
+    # here we're checking that files are written/aborted as expected
+    for original_record in test_json:
+        if original_record.get('Harvest'):
+
+            # we do this to force an update (and this will happen with every record update anyway)
+            original_record['Harvest']['Dates']['LastSeen'] = now
+
+        record_result = cache_nodes['writer'].write_record(database=test_database,
+                                                           record=original_record,
+                                                           filter_criteria=['unique_id'])
+
+        assert bool(record_result) is original_record['expected_state']
+
+        if record_result:
+            collection_name = cache_nodes['writer'].get_collection_name(**original_record['Harvest'])
+            collection = cache_nodes['writer'][test_database][collection_name]
+
+            new_record = collection.find_one({"_id": record_result}, {"_id": 0})
+
+            from flatten_json import flatten
+
+            new_record = flatten(new_record, separator='.')
+            original_record = flatten(original_record, separator='.')
+
+            # we expect every field except Dates.LastSeen to be the same
+            for key, value in new_record.items():
+                if key == 'Dates.LastSeen':
+                    assert value.replace(tzinfo=None) == now
+
+                else:
+                    assert value == original_record[key]
+
