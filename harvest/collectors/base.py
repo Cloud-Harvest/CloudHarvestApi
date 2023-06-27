49dcd57685aac0f02aa5191d3e2968c0a61607ba
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-06-27 13:50:00 -0400

    added collection files

---
+++ harvest/collectors/base.py
@@ -0,0 +1,9 @@
+class Collector:
+    def __init__(self):
+        pass
+
+
+class Step:
+    def __init__(self):
+        pass
+
---
+++ harvest/collectors/threading.py
@@ -0,0 +1,3 @@
+class ThreadPool:
+    def __init__(self):
+        pass
