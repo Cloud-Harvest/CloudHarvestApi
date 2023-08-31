dad52ca0df4e0f5334d9535d85246467e09b369a
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-08-30 23:51:12 -0400

    added basic report class

--- harvest/reporting.py
+++ harvest/reporting.py
@@ -1,6 +1,35 @@
-class ReportRegister:
-    reports = []
 
 class Report:
-    def __init__(self, name: str, ):
-        pass
+    def __init__(self, report_configuration: dict, match: tuple = (), add: tuple = (), limit: int = None, order: tuple = (), **kwargs):
+        self.report_configuration = report_configuration
+        self.add = add
+        self.match = match
+        self.limit = limit
+        self.order = order
+
+        self.results = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return None
+
+    def build(self) -> dict:
+        from registry import Registry
+
+        results = []
+
+        for task_name, task_configuration in self.report_configuration.items():
+            task_class = Registry.get_module(task_name)(**task_configuration)
+
+        return {
+            "meta": {
+                "StartTime": None,
+                "EndTime": None,
+                "Count": 0
+            },
+            "results": None,
+        }
+
+class ReportStatusCodes:
