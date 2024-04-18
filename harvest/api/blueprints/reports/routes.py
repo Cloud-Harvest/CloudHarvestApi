d732f85e8f228a99a5e9e270b498d741fb05889a
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-04-18 16:39:28 -0400

    updated function based on TaskChain criteria

--- harvest/api/blueprints/reports/routes.py
+++ harvest/api/blueprints/reports/routes.py
@@ -11,28 +11,41 @@ blueprint = Blueprint(
 def reports_run() -> Response:
     request_json = loads(request.get_json())
 
+    report_name = request_json.get('report_name')
     from configuration import HarvestConfiguration
 
-    if request_json.get('report_name_or_file') not in HarvestConfiguration.reports.keys():
-        return jsonify({'error': f'report `{request_json.get("report_name_or_file")}` not found'})
+    if report_name not in HarvestConfiguration.reports.keys():
+        return jsonify([{'error': f'report `{request_json.get("report_name")}` not found'}])
 
-    from .resources import Report
-    with Report(**request_json) as report:
-        report.build()
+    report_configuration = HarvestConfiguration.reports.get(report_name)
 
-    return jsonify({})
+    from tasks.chains import task_chain_from_dict
+    report = task_chain_from_dict(task_chain_name=report_name,
+                                  task_chain=report_configuration,
+                                  chain_class_name='report')
+
+    report.run()
+
+    result = report.result
+
+    return jsonify(result)
 
 
 @blueprint.route('/reports/list', methods=['GET'])
 def reports_list() -> Response:
     from configuration import HarvestConfiguration
     # local api reports
-    result = [
-        {
-            'name': k,
-            'description': v.get('description', 'no description')
-         }
-        for k, v in HarvestConfiguration.reports.items()
-    ]
+    result = {
+        'data': [
+            {
+                'Name': k,
+                'Description': v.get('description', 'no description')
+            }
+            for k, v in HarvestConfiguration.reports.items()
+        ],
+        'meta': {
+            'headers': ['Name', 'Description']
+        }
+    }
 
     return jsonify(result)
