d7534cad93717d82165bfe221e0c81329a18830b
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-08 14:23:10 -0400

    replaced poetry with setuptools

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,8 @@ ENV PIP_ROOT_USER_ACTION=ignore
 
 COPY . .
 
-RUN pip install -r ./requirements.txt
+# TODO: add pytest tests/ to the RUN command
+RUN pip install setuptools \
+    && python -m pip install .
 
-RUN mkdir -pv /etc/harvest.d/
-
-# copy the default harvest.yaml unless it already exists (previously mounted)
-RUN cp -vn /src/harvest/harvest.yaml /etc/harvest.d/harvest.yaml
-
-RUN chmod 600 /etc/harvest.d/*
-
-ENTRYPOINT python harvest/wsgi.py
+ENTRYPOINT python CloudHarvestApi/wsgi.py
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+config = dict(name='CloudHarvestApi',
+              version='0.1.0',
+              description='This is the API layer for CloudHarvest.',
+              author='Cloud Harvest, Fiona June Leathers',
+              author_email='email@cloudharvest.com, fiona.june.leathers@example.com',
+              url='https://github.com/Cloud-Harvest/CloudHarvestApi',
+              packages=find_packages(include=['CloudHarvestApi']),
+              install_requires=required,
+              license='CC Attribution-NonCommercial-ShareAlike 4.0 International')
+
+setup(**config)
