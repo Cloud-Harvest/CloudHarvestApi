d80ba217dd41bf080a9e89b3289b9f3056acac04
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-10 17:50:46 -0400

    moved setup meta to meta.py

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup, find_packages
+from CloudHarvestApi.meta import meta
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-config = dict(name='CloudHarvestApi',
-              version='0.1.0',
-              description='This is the API layer for CloudHarvest.',
-              author='Cloud Harvest, Fiona June Leathers',
-              url='https://github.com/Cloud-Harvest/CloudHarvestApi',
-              packages=find_packages(include=['CloudHarvestApi']),
-              install_requires=required,
-              classifiers=[
-                  'Programming Language :: Python :: 3.12',
-              ],
-              license='CC Attribution-NonCommercial-ShareAlike 4.0 International')
+config = dict(packages=find_packages(include=['CloudHarvestApi']),
+              install_requires=required)
+
+config = config | meta
 
 
 def main():
