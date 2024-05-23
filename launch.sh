3b928c98bc69ac19d333679cae21123335cdca2c
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-22 22:36:04 -0400

    added --version
    cleaned up some code
    
    added step to create default log file under the current user

--- launch.sh
+++ launch.sh
@@ -3,6 +3,7 @@
 # Initialize our own variables
 with_mongo=0
 harvest_config=0
+version=0
 image_name="fionajuneleathers/cloud-harvest-api"
 image_tag="latest"
 
@@ -28,6 +29,10 @@ do
         image_name="$1"  # Assign the next argument as the image name
         shift # Remove the image name from processing
         ;;
+        --version)
+        version=1
+        shift # Remove --version from processing
+        ;;
         --help)
         echo "Usage: ./launch.sh [--with-mongo] [--harvest-config] [--tag] [--image] [--help]"
         echo ""
@@ -45,13 +50,36 @@ do
     esac
 done
 
+# If --version was provided, print the version from /src/meta.json
+if [ $version -eq 1 ]; then
+    version_info=$(docker run --rm \
+    --entrypoint=/bin/bash \
+    "$image_name:$image_tag" \
+    -c "version_number=\$(grep '\"version\"' /src/meta.json | cut -d '\"' -f 4 | tr -d '\n'); \
+        commit_hash=\$(cd /src && git rev-parse --short HEAD); \
+        branch_name=\$(cd /src && git rev-parse --abbrev-ref HEAD); \
+        echo \"\$version_number-\$commit_hash(\$branch_name)\"")
+    echo "CloudHarvestApi v$version_info"
+    exit 0
+fi
+
 # Check if the app/harvest.json file exists or --harvest-config is provided
 if [ ! -f "./app/harvest.json" ] || [ $harvest_config -eq 1 ]; then
     # If the file does not exist or --harvest-config is provided, start config.py using docker run
-    docker run -it --rm --entrypoint=/bin/bash -v "./app:/src/app" "$image_name:$image_tag" -c "python3 config.py"
+    docker run -it --rm \
+        -v "./app:/src/app" \
+        --entrypoint=/bin/bash \
+        --user "$(id -u):$(id -g)" \
+        "$image_name:$image_tag" \
+        -c "
+          source /venv/bin/activate &&
+          python3 config.py
+          "
 
     # Check the exit status of config.py
-    if [ $? -ne 0 ]; then
+    if [ $? -eq 0 ]; then
+        echo "Configuration completed successfully."
+    else
         # If the exit status is not 0, abort the script
         echo "config.py exited with an error. Aborting."
         exit 1
@@ -62,6 +90,12 @@ if [ ! -f "./app/harvest.json" ] || [ $harvest_config -eq 1 ]; then
         echo "--harvest-config was specified. Exiting."
         exit 0
     fi
+fi
+
+if [ ! -d "./app/logs" ] || [ ! -f "./app/logs/api.log" ]; then
+    echo "Creating log directory and log file."
+    mkdir -p ./app/logs
+    touch ./app/logs/api.log
 fi
 
 export LOCAL_UID=$(id -u)
