166479086c5be71d45b6ba50bdd157a4024dc464
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-06-11 13:14:20 -0400

    Added log output for HarvestBlueprint

--- CloudHarvestApi/api/blueprints/base.py
+++ CloudHarvestApi/api/blueprints/base.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 from CloudHarvestCorePluginManager.decorators import register_instance
+from logging import getLogger
+
+logger = getLogger('harvest')
 
 
 @register_instance
 class HarvestBlueprint(Blueprint):
     def __init__(self, *args, **kwargs):
+        logger.info(f'Initializing Blueprint: {args[0]}')
+
         super().__init__(*args, **kwargs)
 
