b654deb2d4a1ef1edfd9ac9fb1c3e2012fb79c4c
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-06-11 13:15:46 -0400

    Added custom JSONProvider which returns datetime objects in ISO format

--- CloudHarvestApi/wsgi.py
+++ CloudHarvestApi/wsgi.py
@@ -1,12 +1,27 @@
+from flask.json.provider import DefaultJSONProvider
+
+
+class UpdatedJSONProvider(DefaultJSONProvider):
+    # adopted from https://stackoverflow.com/a/74618781
+    def default(self, o):
+        if isinstance(o, (date, datetime)):
+            return o.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        return super().default(o)
+
+
 class CloudHarvestApi:
     app = None
 
     @staticmethod
     def run(**kwargs):
+        CloudHarvestApi.app.json = UpdatedJSONProvider(CloudHarvestApi.app)
         CloudHarvestApi.app.run(**kwargs)
 
 
 if __name__ == '__main__':
+    from datetime import datetime, date
+
     from api.launch import init_app
     from configuration import HarvestConfiguration
     from CloudHarvestCorePluginManager.registry import Registry
