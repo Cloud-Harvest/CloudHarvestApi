69c51a25b5e58248fd5708d5453d0c808ab00d94
Fiona June Leathers <fiona.june.leathers@outlook.com>
2025-04-15 12:03:19 -0400

    added `pstar/list_platforms` and `pstar/list_services`

--- CloudHarvestApi/blueprints/pstar.py
+++ CloudHarvestApi/blueprints/pstar.py
@@ -126,3 +126,80 @@ def list_platform_regions(platform: str) -> Response:
         task_result = queue_result.json
 
     return safe_jsonify(**task_result)
+
+@pstar_blueprint.route(rule='/list_platforms', methods=['GET'])
+def list_platforms() -> Response:
+    """
+    List the available platforms by retrieving them from the agent configurations.
+    :return: A response.
+    """
+
+    from CloudHarvestCoreTasks.silos import get_silo
+    from json import loads
+
+    silo = get_silo('harvest-nodes')
+    client = silo.connect()
+    agents = client.keys('agent*')
+
+    result = []
+    message = 'OK'
+
+    try:
+        platforms = []
+        [
+            platforms.extend(loads(client.get(agent)).get('accounts') or [])
+            for agent in agents
+        ]
+
+        # Remove duplicates
+        platforms = sorted(list(set(platforms)))
+
+        result = [
+            {
+                'platform': platform.split(':')[0]
+            }
+            for platform in platforms
+            if platform is not None and ':' in platform
+        ]
+
+    except Exception as ex:
+        message = f'Failed to list available accounts with error: {str(ex)}'
+
+    finally:
+        return safe_jsonify(
+            success=True if message == 'OK' else False,
+            reason=message,
+            result=result
+        )
+
+@pstar_blueprint.route(rule='/list_services', methods=['GET'])
+def list_services() -> Response:
+    """
+    List the available services.
+
+    Returns:
+    """
+
+    from CloudHarvestApi.blueprints.tasks import list_available_templates
+
+    services = []
+    templates = list_available_templates().json.get('result') or []
+
+    message = 'OK'
+
+    try:
+        for template in templates:
+            template_category, template_name = template.split('/')
+            _, template_category = template_category.split('_')
+
+            if template_category == 'services':
+                services.append(template_name)
+
+    except Exception as ex:
+        message = f'Failed to list available services with error: {str(ex)}'
+
+    return safe_jsonify(
+        success=True if message == 'OK' else False,
+        reason=message,
+        result=sorted(list(set(services)))
+    )
