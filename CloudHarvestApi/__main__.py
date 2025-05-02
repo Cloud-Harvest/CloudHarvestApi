da7f11ca57447be3101a97e108fef2b0044e6d61
Fiona June Leathers <fiona.june.leathers@outlook.com>
2025-05-02 10:50:40 -0400

    simplified node identification

--- CloudHarvestApi/__main__.py
+++ CloudHarvestApi/__main__.py
@@ -46,6 +46,12 @@ else:
 config = WalkableDict(**load_configuration_from_file())
 config['api']['connection'] = vars(args)
 config['api']['pid'] = getpid()
+config['api']['name'] = ':'.join([
+    'agent',
+    args.host,
+    str(args.port),
+    str(getpid())
+])
 
 # Makes the configuration available throughout the app
 Environment.merge(config)
--- CloudHarvestApi/startup.py
+++ CloudHarvestApi/startup.py
@@ -92,7 +92,7 @@ def start_node_heartbeat(config: WalkableDict):
                 "version": app_metadata.get('version')
             }
 
-        node_record_identifier = f'{node_role}:{node_name}:{node_info["port"]}:{node_info["pid"]}'
+        node_record_identifier = config.walk('api.name')
 
         def format_for_redis(dictionary: dict) -> dict:
             """
